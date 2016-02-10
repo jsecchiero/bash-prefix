6d0ef0ac907696b952dfc5e8e40d14c6625ef969
SACO9788 <jacopo.secchiero.guest@saipem.com>
2016-02-10 08:57:00 +0100

    Organize file tree moving core functions in lib

---
+++ lib/core/prefix.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+prefix() {
+    export pre=${@}
+    export PS1="$pre> "
+
+    [[ -f $PREFIX_HOME/lib/bash-preexec/bash-preexec.sh ]] && source $PREFIX_HOME/lib/bash-preexec/bash-preexec.sh
+    preexec() { $pre $1; kill -INT $$; };
+    export -f preexec
+}
+
+export -f prefix
