fd01f332fef86561a87ed718ca3a72ca36273527
jsecchiero <jacopo.secchiero.guest@saipem.com>
2016-02-11 08:48:33 +0100

    Fix escaping behaviour

--- lib/core/prefix.sh
+++ lib/core/prefix.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 prefix() {
-    export pre=${@}
-    export PS1="$pre> "
+    export pre="${@}"
+    export PS1="$(echo '${pre}')> "
 
     [[ -f $PREFIX_HOME/lib/bash-preexec/bash-preexec.sh ]] && source $PREFIX_HOME/lib/bash-preexec/bash-preexec.sh
-    preexec() { $pre $1; kill -INT $$; };
+    preexec() { ${pre} $1; kill -INT $$; };
     export -f preexec
 }
 
